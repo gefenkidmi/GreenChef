45cc656d253bebf914d16d8f508cfc69c61209f6
zivklein21 <zivklein@Mac.lan>
2025-01-04 22:53:56 +0200

    Delete params

--- app/src/main/java/com/example/greenchef/Fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/greenchef/Fragments/FavoritesFragment.kt
@@ -20,16 +20,7 @@ import com.example.greenchef.ViewModels.UserViewModel
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoritesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavoritesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     private lateinit var recipeRecyclerView: RecyclerView
 
     private val recipeViewModel: RecipeViewModel by viewModels()
@@ -37,10 +28,7 @@ class FavoritesFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        arguments?.let {}
     }
 
     override fun onCreateView(
