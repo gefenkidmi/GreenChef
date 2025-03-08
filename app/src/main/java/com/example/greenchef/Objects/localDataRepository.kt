f8459673d0b05b8937da96d2fc0b1ee7182ceee8
zivklein21 <zivklein@Mac.lan>
2025-03-08 13:27:38 +0200

    Add new Ingredients and categories

--- app/src/main/java/com/example/greenchef/Objects/localDataRepository.kt
+++ app/src/main/java/com/example/greenchef/Objects/localDataRepository.kt
@@ -3,14 +3,21 @@ package com.example.greenchef.Objects
 object localDataRepository {
     val ingredients: List<String> = listOf(
         "Fresh Spinach", "Green Apple", "Banana", "Frozen Berries", "Mango", "Pineapple"," Pitted Dates",
-        "Chia Seeds", "Flaxseeds", "Oats", "Walnuts", "Ground Almonds","Shredded Coconut",
-        "Water", "Almond Milk", "Milk", "Coconut Milk","Ice Cubes", "Coconut Oil", "Vanilla Extract",
+        "Bell Peppers", "Zucchini", "Cherry Tomatoes", "Parsley", "Avocado", "Cucumber", "Red Onion", "Cilantro", "Olives",
+        "Cabbage", "Carrots", "Edamame", "Ginger",
+        "Tofu",
+        "Chia Seeds", "Flaxseeds", "Oats", "Walnuts", "Ground Almonds","Shredded Coconut", "Almonds", "Peanuts",
+        "Quinoa", "Green lentils", "Brown Rice",
+        "Water", "Almond Milk", "Milk", "Coconut Milk","Ice Cubes", "Sesame Oil",
+        "Coconut Oil", "Vanilla Extract", "Olive Oil", "Lemon Juice", "Balsamic Vinegar", "Soy Sauce", "Lime Juice",
         "Almond Butter", "Natural Peanut Butter", "Honey", "Maple Syrup", "Raw Cocoa Powder", "Cinnamon",
+        "Chickpeas",
         "Whole Spelt Flour", "Baking Powder",
-        "Egg",
+        "Egg", "Salt", "Pepper", "Oregano", "Sesame",
+        "Feta Cheese",
     )
 
     val categories: List<String> = listOf(
-        "Desserts", "Shakes"
+        "Desserts", "Shakes", "Salads & Bowls"
     )
 }
