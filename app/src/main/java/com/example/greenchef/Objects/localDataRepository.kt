81a7568ff45ce19520d0c208989ffc2b38b374dc
zivklein21 <zivklein@Mac.lan>
2025-03-08 12:03:11 +0200

    Add new Ingredients and categories

--- app/src/main/java/com/example/greenchef/Objects/localDataRepository.kt
+++ app/src/main/java/com/example/greenchef/Objects/localDataRepository.kt
@@ -2,20 +2,15 @@ package com.example.greenchef.Objects
 
 object localDataRepository {
     val ingredients: List<String> = listOf(
-        "Salt", "Sugar", "Flour", "Eggs", "Butter", "Milk", "Chicken", "Rice", "Tomatoes", "Onions",
-        "Garlic", "Olive Oil", "Pasta", "Cheese", "Beef", "Fish", "Lemon", "Bread", "Potatoes", "Avocado",
-        "Cucumber", "Spinach", "Soy Sauce", "Honey", "Basil", "Peanut Butter", "Chocolate", "Vanilla Extract",
-        "Quinoa", "Lentils", "Chickpeas", "Shrimp", "Salmon", "Cilantro", "Paprika", "Mustard", "Ketchup",
-        "Mayonnaise", "Bell Pepper", "Cumin", "Thyme", "Rosemary", "Ginger", "Turmeric", "Cinnamon", "Nutmeg",
-        "Chili Powder", "Coriander", "Cardamom", "Sesame Seeds", "Walnuts", "Almonds", "Pineapple", "Mango",
-        "Coconut Milk", "Curry Paste", "Tofu", "Zucchini", "Pumpkin", "Sweet Potato", "Peas", "Asparagus",
-        "Artichoke", "Pesto", "Prosciutto", "Salami", "Parmesan", "Cheddar", "Blueberries", "Raspberries",
-        "Strawberries", "Bananas", "Peaches", "Apples", "Oranges", "Grapes", "Kiwi", "Melon", "Watermelon",
-        "Pomegranate", "Cranberries", "Pine Nuts", "Cashews", "Pecans", "Hazelnuts", "Eggplant", "Cabbage",
-        "Cauliflower", "Brussels Sprouts", "Bok Choy", "Radish", "Feta Cheese", "Gouda", "Sausage", "Turkey",
+        "Fresh Spinach", "Green Apple", "Banana", "Frozen Berries", "Mango", "Pineapple"," Pitted Dates",
+        "Chia Seeds", "Flaxseeds", "Oats", "Walnuts", "Ground Almonds","Shredded Coconut",
+        "Water", "Almond Milk", "Milk", "Coconut Milk","Ice Cubes", "Coconut Oil", "Vanilla Extract",
+        "Almond Butter", "Natural Peanut Butter", "Honey", "Maple Syrup", "Raw Cocoa Powder", "Cinnamon",
+        "Whole Spelt Flour", "Baking Powder",
+        "Egg",
     )
 
     val categories: List<String> = listOf(
-        "American", "Italian", "Asian", "Mexican", "Vegan", "Mediterranean", "Indian", "French", "Thai", "Japanese"
+        "Desserts", "Shakes"
     )
 }
